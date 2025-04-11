fb87e4b9e4bd2a38a90a033a5675c6fe0b642822
Rafael Fuzii <yuzofuzii@gmail.com>
2025-04-11 20:21:16 -0300

    adicionado dto

--- src/personagem/dto/create-personagem.dto.ts
+++ src/personagem/dto/create-personagem.dto.ts
@@ -1,1 +1,26 @@
-export class CreatePersonagemDto {}
+import { IsString, IsNotEmpty, IsEnum, IsInt, Max } from "class-validator";
+import classes from "../enum/classes.enum";
+
+export class CreatePersonagemDto {
+    @IsString()
+    id: string;
+    nome: string;
+
+    @IsString()
+    @IsNotEmpty()
+    NomeAventureiro: string;
+
+    @IsNotEmpty()
+    @IsEnum(classes)
+    classe: classes;
+
+    @IsInt()
+    level: number;
+
+    listaItemMagico: Array<string>;
+
+    @IsInt()
+    @Max(10)
+    forca: number;
+    defesa: number
+}
